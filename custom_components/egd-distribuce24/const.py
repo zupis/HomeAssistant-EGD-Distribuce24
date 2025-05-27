60eb1478b335d586b0ca7e03e448a9ad71b2d4dd
Ondřej Župka <46270764+zupis@users.noreply.github.com>
2025-05-27 16:00:15 +0200

    Update const.py

--- custom_components/egd-distribuce24/const.py
+++ custom_components/egd-distribuce24/const.py
@@ -3,7 +3,7 @@
 from datetime import timedelta
 
 # Integration domain
-DOMAIN = "egd-distribuce24"
+DOMAIN = "egd_distribuce24"
 
 # Configuration keys
 CONF_CLIENT_ID = "client_id"
@@ -11,8 +11,8 @@ CONF_CLIENT_SECRET = "client_secret"
 CONF_EAN = "ean"
 CONF_DAYS_OFFSET = "days_offset" 
 CONF_MAX_FETCH_DAYS = "max_fetch_days"
-CONF_CREATE_CONSUMPTION_SENSORS = "create_consumption_sensors" # New
-CONF_CREATE_PRODUCTION_SENSORS = "create_production_sensors" # New
+CONF_CREATE_CONSUMPTION_SENSORS = "create_consumption_sensors"
+CONF_CREATE_PRODUCTION_SENSORS = "create_production_sensors"
 
 # API URLs
 TOKEN_URL = "https://idm.distribuce24.cz/oauth/token"
@@ -25,18 +25,17 @@ PROFILE_CONSUMPTION_ENERGY = "ICQ2"
 PROFILE_PRODUCTION_ENERGY = "ISQ2"  
 
 # Default values
-DEFAULT_DAYS_OFFSET = 2 
+DEFAULT_DAYS_OFFSET = 1 
 DEFAULT_MAX_FETCH_DAYS = 3 
-DEFAULT_SCAN_INTERVAL = timedelta(hours=23) 
-DEFAULT_CREATE_CONSUMPTION_SENSORS = True # New
-DEFAULT_CREATE_PRODUCTION_SENSORS = True  # New
+DEFAULT_SCAN_INTERVAL = timedelta(hours=4) # Changed from 23 hours
+DEFAULT_CREATE_CONSUMPTION_SENSORS = True
+DEFAULT_CREATE_PRODUCTION_SENSORS = False
 
 # Attributes used in sensors
 ATTR_LAST_UPDATE_STATUS = "last_update_status"
 ATTR_FIFTEEN_MINUTE_DATA = "fifteen_minute_data"
 ATTR_API_DATA_POINTS_RECEIVED = "api_data_points_received"
 ATTR_DATA_FETCHED_FOR_DATE_LOCAL = "data_fetched_for_date_local"
-# ... (ostatní atributy zůstávají)
 ATTR_DATA_RANGE_UTC_FROM = "data_range_utc_from"
 ATTR_DATA_RANGE_UTC_TO = "data_range_utc_to"
 ATTR_LAST_SUCCESSFUL_FETCH_UTC = "last_successful_fetch_utc"
@@ -56,4 +55,3 @@ API_PAGE_SIZE = 3000
 
 # Platforms
 PLATFORMS = ["sensor"]
-
